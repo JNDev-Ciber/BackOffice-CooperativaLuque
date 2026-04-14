86f01a460d56bfb7d3b5f9fc9d3fdd06db2162ec
Juani Bertossi <bertossijuani@gmail.com>
2026-04-14 14:06:16 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from "./pages/LoginPage";
 import NewsPage from "./pages/NewsPage";
 import InternetPlansPage from "./pages/InternetPlansPage";
 import ElectricityPlansPage from "./pages/ElectrityPage";
-
+//
 function App() {
   return (
     <Routes>
